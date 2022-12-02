dfa95af3c81e0278842a1529372b90a0910b8d0c
Ryan Ostrander <ryan08@uw.edu>
2022-12-01 16:59:00 -0800

    updated app

--- scripts/application.py
+++ scripts/application.py
@@ -3,24 +3,33 @@ import streamlit as st
 import time
 
 data = st.file_uploader("Choose file to upload:")
-limit = 200
 
-if data is not None:
+if data is None:
+	st.write("Please upload a dataset.")
+elif st.button("Upload"):
 	df = functions.upload(data)
+	st.write("Dataset available for use...")
+
+	limit = st.number_input("Set row limit: ", 1, len(df), 100)
 	df_keep = functions.format_col(df)[:limit]
-	st.write("Dataset available for use.")
 
 	a = st.number_input("Enter first mass adjustment:", 0, 80, 5)
 	b = st.number_input("Enter second mass adjustment:", 0, 80, 11)
 	if b<a:
 		raise ValueError("b must be larger than a")
 
-else:
-	st.write("Please upload a dataset.")
+	if st.button("Find Pairs", help="Click to run pair picking algorithm"):
+		start = time.time()
+		pairs = functions.pick_pairs(df_keep, a, b)
+		end = time.time()
+		st.write("Compound Pairs found.")
+		st.write("Runtime: ", end-start, "s")
 
 choices = [
 	"What do you want to do?",
+	"See Full Dataset",
 	"Show Compound Pairs",
+	"Show Data for Compound Pairs",
 	"Adjust Masses of Pairs",
 	"ID Lipids",
 	"Visualize"
@@ -28,29 +37,23 @@ choices = [
 choice = st.selectbox("Pick One:", choices)
 
 if choice == choices[1]:
-	start = time.time()
-	df_pairs = functions.pick_pairs(df_keep, a, b)
-	end = time.time()
-	st.write("Runtime: ", end-start, "s")
-	st.write(df_pairs)
-else: 
-	pass
+	st.write(df)
 
 if choice == choices[2]:
+	st.write(pairs)
+
+if choice == choices[3]:
+	st.write(df_keep[pairs.flatten()])
+
+if choice == choices[4]:
 	pairs = functions.pick_pairs(df_keep, a, b)
 	df_adjusted = functions.mass_adj(pairs, df_keep, a, b)
 	st.write(df_adjusted)
-else:
-	pass
 
-if choice == choices[3]:
+if choice == choices[5]:
 	# Lipidomics database
 	pass
-else:
-	pass
 
-if choice == choices[4]:
+if choice == choices[6]:
 	# Visual of lipids???
-	pass
-else: 
 	pass
