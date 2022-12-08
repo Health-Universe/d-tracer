71554e53bf9ebef7b3ae7b38e47b27083669b13d
Alyson Shoji <ashoji@uw.edu>
2022-12-07 21:54:27 -0800

    added smoke test to test_format_col.py

--- d_tracer/tests/test_format_col.py
+++ d_tracer/tests/test_format_col.py
@@ -1,1 +1,25 @@
 """unit test to test functions.format_col"""
+
+import unittest
+import unittest.mock as mock
+import numpy as np
+import functions
+import pathlib
+import pandas as pd
+import sys
+
+my_data_path = pathlib.Path(__file__).parents[2].joinpath("examples/example_input.csv")
+
+class TestColumn(unittest.TestCase):
+    """
+    Test for upload function
+    """
+
+    def test_smoke(self):
+        """
+        smoke test to make sure column function runs
+        """
+        functions.upload(my_data_path,limit=None)
+        functions.format_col(my_data_path, samples=None)
+
+        
